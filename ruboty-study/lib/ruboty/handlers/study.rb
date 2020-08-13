a848074d501c7e4fd49c2c6a15537b464b4fa0ea
zeero <zeero26@gmail.com>
2020-08-13 11:38:57 +0900

    [2-2] feat: hello world 英日西仏対応

--- ruboty-study/lib/ruboty/handlers/study.rb
+++ ruboty-study/lib/ruboty/handlers/study.rb
@@ -3,7 +3,7 @@ require 'ruboty/study'
 module Ruboty
   module Handlers
     class Study < Base
-      on(/hello/, name: 'hello', description: 'helloと言ったら、world!を返す')
+      on(/(?<hello>hello|こんにちは|hola|bonjour)/, name: 'hello', description: 'helloと言ったら、world!を返す（英日西仏対応版）')
 
       def hello(message)
         Ruboty::Study::Actions::Hello.new(message).call
--- ruboty-study/lib/ruboty/study/actions/hello.rb
+++ ruboty-study/lib/ruboty/study/actions/hello.rb
@@ -5,7 +5,24 @@ module Ruboty
     module Actions
       class Hello < Ruboty::Actions::Base
         def call
-          message.reply('world!')
+          hello = message[:hello]
+          world = hello_world(hello)
+          message.reply("#{world}!")
+        end
+
+        private
+
+        def hello_world(source)
+          case source
+          when 'hello'
+            'world'
+          when 'こんにちは'
+            '世界'
+          when 'hola'
+            'Mundo'
+          when 'bonjour'
+            'le monde'
+          end
         end
       end
     end
