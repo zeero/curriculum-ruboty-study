679505ae4e6799f0d9689ca62617586648706dc9
zeero <zeero26@gmail.com>
2020-08-13 11:04:43 +0900

    [1-3] feat: hello world 実装

--- ruboty-study/lib/ruboty/handlers/study.rb
+++ ruboty-study/lib/ruboty/handlers/study.rb
@@ -3,7 +3,7 @@ require 'ruboty/study'
 module Ruboty
   module Handlers
     class Study < Base
-      on(/study hello/, name: 'hello', description: 'TODO: write your description')
+      on(/hello/, name: 'hello', description: 'helloと言ったら、world!を返す')
 
       def hello(message)
         Ruboty::Study::Actions::Hello.new(message).call
