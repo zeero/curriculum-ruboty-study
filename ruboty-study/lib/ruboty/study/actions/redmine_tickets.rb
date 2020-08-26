704bfa576c52877ec44b9a3a474612d2e93d53b8
zeero <zeero26@gmail.com>
2020-08-26 22:37:09 +0900

    [3-3] feat: Actions::RedmineTickets

---
+++ ruboty-study/lib/ruboty/study/actions/redmine_tickets.rb
@@ -0,0 +1,32 @@
+require 'ruboty/study'
+
+module Ruboty
+  module Study
+    module Actions
+      class RedmineTickets < Ruboty::Actions::Base
+        def call
+          user_id = message[:user]
+
+          # Redmineから特定ユーザのチケット一覧を取得
+          condition = { assigned_to_id: user_id }
+          issues = ActiveResources::Redmine::Issue.where(condition)
+
+          # 返信
+          message.reply(format(user_id, issues))
+        end
+
+        private
+
+        def format(user_id, issues)
+          lines = []
+          lines << "User ID: #{user_id} の担当チケット一覧"
+          lines << "```"
+          lines << issues.map { |issue| "##{issue.id}: #{issue.subject}" }
+          lines << "```"
+          lines.join("\n")
+        end
+      end
+    end
+  end
+end
+
