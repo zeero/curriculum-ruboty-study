79ccfd4bc9b148ae9716b9030ae3575504f0d887
zeero <zeero26@gmail.com>
2020-08-28 10:44:10 +0900

    [4-2] feat: Handlers::Study#redmine_link

--- ruboty-study/lib/ruboty/handlers/study.rb
+++ ruboty-study/lib/ruboty/handlers/study.rb
@@ -7,12 +7,18 @@ module Ruboty
 
       on(/redmine +tickets +(?<user>[0-9]+)/, name: 'redmine_tickets', description: 'Redmineの担当チケット一覧を返す')
 
+      on(/redmine +link +(?<slack_user_name>@\S+) +(?<redmine_user_id>[0-9]+)/i, name: 'redmine_link', description: 'Slackユーザ名とRedmineユーザIDをリンクする')
+
       def hello(message)
         Ruboty::Study::Actions::Hello.new(message).call
       end
 
       def redmine_tickets(message)
         Ruboty::Study::Actions::RedmineTickets.new(message).call
+      end
+
+      def redmine_link(message)
+        Ruboty::Study::Actions::RedmineLink.new(message).call
       end
     end
   end
